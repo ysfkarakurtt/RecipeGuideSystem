d23f751676d4ca6ee35b421d356e8d5c1b006eb0
Yusuf Karakurt <yusufkarakurtt@outlook.com>
2025-03-22 21:31:22 +0300

    updated

--- App.java
+++ App.java
@@ -7,11 +7,6 @@ import java.sql.SQLException;
 public class App {
     public static void main(String[] args) throws SQLException {
         Helper.setTheme();
-
-       // Background bg = new Background("C:\\Users\\Yusuf\\IdeaProjects\\RecipeGuide\\src\\img\\background.jpg");
         HomepageUI homepageUI = new HomepageUI();
-
-       // homepageUI.add(bg);
-
     }
 }
